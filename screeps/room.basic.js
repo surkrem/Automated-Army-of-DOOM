c0a0ee54ec698610c3776e780fe0177d4565c6c7
surkrem <surkrem@gmail.com>
2017-12-30 19:43:21 -0800

    Update room.basic.js

--- screeps/room.basic.js
+++ screeps/room.basic.js
@@ -4,8 +4,14 @@ let roomBasic = {
     run: function(room) {
         if (room.controller.my) {
             let spawns = room.find(FIND_MY_SPAWNS);
-            if (spawns.length < 1) {
-                Room.createConstructionSite(25, 23, STRUCTURE_SPAWN);
+            if (room.controller.level >= 8 && spawns.length < 3) {
+                Room.createConstructionSite(28, 27, STRUCTURE_SPAWN);
+            }
+            else if (room.controller.level >= 7 && spawns.length < 2) {
+                Room.createConstructionSite(22, 27, STRUCTURE_SPAWN);
+            }
+            else if (room.controller.level > 0 && spawns.length < 1) {
+                Room.createConstructionSite(25, 22, STRUCTURE_SPAWN);
             }
         }
     }
